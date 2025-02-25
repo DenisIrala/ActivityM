356f83560eeb561d9536a1e88f535788809be2f8
Denis Irala <adrian82009@hotmail.com>
2025-02-25 17:20:09 -0500

    access env var

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 const port = 3000;
+const DBP = process.env.DB_PASSWORD;
+
 app.use(express.static(path.join(__dirname, '../dist')))
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
