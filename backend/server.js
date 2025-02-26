d06f478bd9727f116ba977b98c711f74896f1afc
skeeper12 <adrian82009@hotmail.com>
2025-02-25 19:54:23 -0500

    test

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,36 @@ const app = express();
 const port = 3000;
 const DBP = process.env.DB_PASSWORD;
 
+const mysql = require('mysql2/promise');
+
+
+const config = {
+  host: 'localhost',
+  user: 'typh',
+  password: process.env.DB_PASSWORD,
+  database: 'ActivityM'
+};
+
+async function main() { //Testing database
+  let connection;
+  try {
+    // Create a connection
+    connection = await mysql.createConnection(config);
+    
+    console.log('Successfully connected to the database');
+
+  } catch (error) {
+    console.error('Database error:', error);
+  } finally {
+    // Close the connection
+    if (connection) await connection.end();
+  }
+}
+
+main();
+
+//Endpoints
+
 app.use(express.static(path.join(__dirname, '../dist')))
 
 app.get('/', (req, res) => {
