4fb2268de8dd9c798825922a4e935accbb2da335
skeeper12 <adrian82009@hotmail.com>
2025-02-25 19:45:57 -0500

    test

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,16 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 const port = 3000;
+const mysql = require('mysql2/promise');
+
+
+const config = {
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+};
+
 app.use(express.static(path.join(__dirname, '../dist')))
 app.get('/', (req, res) => {
   res.send('Hello World!');
