78db8b10c993333032e27134e8d6f593f8ed13ed
Kami North <38385422+Nutik-Wulf@users.noreply.github.com>
2025-03-03 21:35:41 -0500

    Create DBManagement.sql

---
+++ backend/DBManagement.sql
@@ -0,0 +1,29 @@
+USE ActivityM;
+
+-- Create Tables [Ollie]
+
+CREATE TABLE IF NOT EXISTS Accounts (
+	accID INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(30) NOT NULL UNIQUE,
+	pass VARCHAR(30) NOT NULL,
+	PRIMARY KEY(accID)
+);
+
+CREATE TABLE IF NOT EXISTS Lists (
+	listID INT NOT NULL AUTO_INCREMENT,
+	ownerID INT NOT NULL,
+	PRIMARY KEY (listID),
+	FOREIGN KEY (ownerID) REFERENCES Accounts(accID)
+);
+
+CREATE TABLE IF NOT EXISTS Items (
+	itemID INT NOT NULL,
+	listID INT NOT NULL,
+	time DATE,
+	description VARCHAR(100),
+	PRIMARY KEY (itemID),
+	FOREIGN KEY (listID) REFERENCES Lists(listID)
+);
+
+-- Log In / Register [Lindsey]
+
