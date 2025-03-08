aa6a26896dad040f026f64dd9e19f4617406feef
Kami North <38385422+Nutik-Wulf@users.noreply.github.com>
2025-03-08 10:36:47 -0500

    Update DBManagement.sql
    Lindsey's SQL Template for list functions added

--- backend/DBManagement.sql
+++ backend/DBManagement.sql
@@ -28,4 +28,30 @@ CREATE TABLE IF NOT EXISTS Items (
 	FOREIGN KEY (listID) REFERENCES Lists(listID)
 );
 
+-- List Management Functions (SQL Base) [Lindsey]
+-- Ollie will be formatting these into the API.
+CREATE PROCEDURE IF NOT EXISTS getLists (IN accountID INT) 
+BEGIN 
+    SELECT listID, listName 
+    FROM     Lists 
+    WHERE ownerID = accountID; 
+END;
 
+CREATE PROCEDURE IF NOT EXISTS addList (IN accountID INT, IN listName VARCHAR(60)) 
+BEGIN 
+	INSERT INTO Lists (ownerID, ListName)
+	VALUES (accountID, listName); 
+END; 
+
+CREATE PROCEDURE updateList(IN listID INT, IN accountID INT, IN newName VARCHAR(60))
+BEGIN
+	UPDATE Lists
+	SET ListName = newName
+	WHERE listID = listID AND ownerID = accountID;
+END; 
+
+CREATE PROCEDURE deleteList(IN listID INT, IN accountID INT) 
+BEGIN 
+    DELETE FROM Lists
+    WHERE listID = listID AND ownerID = accountID; 
+END;
